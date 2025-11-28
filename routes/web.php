ffe9f08dfd528fda69d704ff5e35f04d67fd31a9
MRSuhada <mochriansuhada2@gmail.com>
2025-11-28 15:55:51 +0700

    Add CRUD Category feature

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -28,5 +29,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::resource('/products', ProductController::class);
+
+Route::resource('/category', CategoryController::class);
 
 require __DIR__ . '/auth.php';
